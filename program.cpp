d27efe267c09ae76ee23bb8812aff469b8836e01
Prathmdeore <144782372+Prathmdeore@users.noreply.github.com>
2023-09-12 14:30:12 +0530

    Create program.cpp

---
+++ program.cpp
@@ -0,0 +1,14 @@
+#include<iostram.h>'
+#include<string.h>
+using namespace std ;
+int main()
+{
+  int roll;
+
+  cout<<"enter your roll number"<<endl;
+  cin>>roll;
+  cout<<"Displaying your roll number "<<endl;
+  cout<<roll<<endl;
+ 
+
+}
